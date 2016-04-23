306c23075e5192c81506a1bea1290b6875ba9f96
Aka Kherkeladze <akaki.kherkeladze@gmail.com>
2016-04-23 20:25:03 +0400

    add/configure bower an gulp. create front-end folder structure and install bootstrap / angular

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var htmlMin = require('gulp-htmlmin');
+
+gulp.task('indexFile', function() {
+    gulp.src('./public/src/index.html').
+        pipe(htmlMin({ collapseWhitespace: true })).pipe(gulp.dest('./public/generated/'));
+});
+
+gulp.task('pages', function() {
+    gulp.src('./public/src/pages/*.html').
+    pipe(htmlMin()).pipe(gulp.dest('./public/generated/pages/'));
+});
+
+gulp.task('scripts', function() {
+    gulp.src('./public/src/js/*.js').
+    pipe(uglify({ mangle: false })).pipe(gulp.dest('./public/generated/js/'));
+});
+
+
+gulp.task('watcher', function () {
+    gulp.watch('./public/src/index.html', ['indexFile']);
+    gulp.watch('./public/src/pages/*.html', ['pages']);
+    gulp.watch('/public/src/pages/*.js', ['scripts']);
+});
